fa5e84f2c704ec6951eac8b0f026e09e4f2e036a
syfr456 <sekarsyafira225@gmail.com>
2023-03-05 17:00:13 +0700

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,107 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" v-if="data !== null">
+    <div v-if="data.category === 'men\'s clothing'" class="blue_tealish">
+      <MenPage
+        :isLoading="isLoading"
+        :data="data"
+        @nextProduct="addPageIndex"
+      />
+    </div>
+
+    <div v-if="data.category == 'women\'s clothing'" class="pink_lace">
+      <WomenPage
+        :isLoading="isLoading"
+        :data="data"
+        @nextProduct="addPageIndex"
+      />
+    </div>
+
+    <div
+      v-if="
+        data.category !== 'women\'s clothing' &&
+        data.category !== 'men\'s clothing'
+      "
+      class="grey_light"
+    >
+      <UnavailablePage :isLoading="isLoading" @nextProduct="addPageIndex" />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MenPage from "./components/MenPage.vue";
+import WomenPage from "./components/WomenPage.vue";
+import UnavailablePage from "./components/UnavailablePage.vue";
 
 export default {
-  name: 'App',
+  name: "App",
+  data: function () {
+    return {
+      pageIndex: 1,
+      data: null,
+      isLoading: true,
+    };
+  },
+
+  methods: {
+    async getData(index) {
+      this.isLoading = true;
+      const res = await fetch(`https://fakestoreapi.com/products/${index}`);
+      const finalRes = await res.json();
+      this.isLoading = false;
+      this.data = finalRes;
+    },
+    addPageIndex() {
+      this.pageIndex++;
+      if (this.pageIndex > 20) {
+        this.pageIndex = 1;
+        this.getData(this.pageIndex);
+      } else {
+        this.getData(this.pageIndex);
+      }
+    },
+  },
+  mounted() {
+    this.getData(this.pageIndex);
+  },
   components: {
-    HelloWorld
-  }
-}
+    MenPage,
+    WomenPage,
+    UnavailablePage,
+  },
+};
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  height: 100vh;
+}
+
+.pink_lace {
+  padding-top: 60px;
+  background-color: #fde2ff;
+  background-image: url(./assets/backgroundPattern.png);
+  height: 60%;
+}
+.blue_tealish {
+  padding-top: 60px;
+  background-color: #d6e6ff;
+  background-image: url(./assets/backgroundPattern.png);
+  height: 60%;
+}
+.grey_light {
+  padding-top: 60px;
+  background-color: #dcdcdc;
+  height: 60%;
 }
 </style>
